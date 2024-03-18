6c26cdef8118e2ed59348630aa52b079dbd59919
kiceng12 <sukmahendrawan12@gmail.com>
2024-03-18 14:28:00 +0700

    looping menggunakan perulangan

---
+++ PerulanganPraktikum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int arr[5];
+
+    for (i = 0; i < 5; ++)
+    {
+
+        cout << 1 << "Hendra" << endl;
+    }
+    
+    for (i = 0; i < 5; i ++){
+        cout << "Masukan Nama = ";
+        cin >> arr{i};
+    }
+
+    for (1 = 0; 1 < 5; 1++){
+        cout << "Name Ke - " << i << " : " << arr[i] << endl;
+    }
+}
